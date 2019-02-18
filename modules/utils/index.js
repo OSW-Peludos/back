0dff526f454e6a2b9f1159f98aaa9dafc0ae34b4
xexuline <jscentella@gmail.com>
2019-02-18 21:08:48 +0100

    added utitlity to generate and send errors

--- modules/utils/index.js
+++ modules/utils/index.js
@@ -1,7 +1,17 @@
+const STATUS = require('http-status');
+
+
 function generateError(msg){
   return new Error(msg)
 }
 
+function errorSender(response, error){
+  console.log(error)
+  response.statusCode = error.code || STATUS.INTERNAL_SERVER_ERROR;
+  response.send({error: error.error})
+}
+
 module.exports = {
   errorGenerator: generateError,
+  errorSender,
 }
